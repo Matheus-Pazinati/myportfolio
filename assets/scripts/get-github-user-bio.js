cdb0e5fdb16a895dacf477623fbda5e6e43c07ef
Matheus Pazinati <matheuspazinati@gmail.com>
2022-06-14 15:57:37 -0300

    Define a descricao da sessao Sobre atraves da bio do Github

---
+++ assets/scripts/get-github-user-bio.js
@@ -0,0 +1,12 @@
+async function getGithubUserProfile() {
+  let req = await fetch('https://api.github.com/users/Matheus-Pazinati')
+  let response = await req.json()
+  setAboutMeContainerDescription(response.bio)
+}
+
+function setAboutMeContainerDescription(text) {
+  let aboutMeContainer = document.querySelector('.about__description')
+  aboutMeContainer.innerHTML = text
+}
+
+getGithubUserProfile()
